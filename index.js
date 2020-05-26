81e3a38b73def0e3cabf5b3ace132e2d106367af
Viren Khandal <virenkhandal@berkeley.edu>
2020-05-26 15:24:19 -0700

    news

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
@@ -23,11 +24,14 @@ app.post('/webhook', (req, res) => {
 
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
     });
 
     // Returns a '200 OK' response to all requests
@@ -65,3 +69,18 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
