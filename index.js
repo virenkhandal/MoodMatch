e8ad325578da6761f0cc254dc64273d228a9ffba
Viren Khandal <virenkhandal@berkeley.edu>
2020-05-26 12:24:44 -0700

    updating token

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = 'IgYAlHrlkI'
+  let VERIFY_TOKEN = 'customToken123'
 
   // Parse the query params
   let mode = req.query['hub.mode'];
