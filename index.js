aaf089fc9a65543393dbb0fa550e29b0e11ac204
Viren Khandal <virenkhandal@berkeley.edu>
2020-05-26 15:46:58 -0700

    updates

--- index.js
+++ index.js
@@ -97,6 +97,19 @@ function callSendAPI(sender_psid, response) {
     },
     "message": response
   }
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
 }
 
 // Handles messaging_postbacks events
