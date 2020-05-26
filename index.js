a26834b6079bf4b2858ff6d0088c2e9003ccfea6
Viren Khandal <virenkhandal@berkeley.edu>
2020-05-26 15:52:54 -0700

    updating index

--- index.js
+++ index.js
@@ -1,70 +1,73 @@
-const PAGE_ACCESS_TOKEN = 'EAAD04ncSFgEBAPhp2o2kwc132nLwjItWjdD0dGcQuv2BIMsaA8JAhDqsVC5NGugWW7dhO3krwpOEaILdhWQvcELDJf6O2tiBBIIQTv6EFvFiVocuuZCD6XsNRNAL7WFSvpoMnYmDodXJJ3cSAxfz71KuraHWoZB1Q84nXsnMvhcFZBZCu1QyGKHhufMaZApYZD';
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var app = express();
+'use strict';
+const PAGE_ACCESS_TOKEN = 'EAAD04ncSFgEBAE8vq6iSppJqpGvIEblzTeyq8SAKE0JJ7aqrPGsYZCAFsq2wSKTbq6zIwzUK0zdA31M6FVF9MYwxFHZC8fdHIYZBqshuGCwZBb743hZC08chwOSiTrJfXCDYVmiWDVvulIl15bqbDFMW95tXWrk6DfZBJST9nnj2K19mSc4LaUR7ZAmBqzFBC4ZD';
+// Imports dependencies and set up http server
+const
+  request = require('request'),
+  express = require('express'),
+  body_parser = require('body-parser'),
+  app = express().use(body_parser.json()); // creates express http server
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.listen((process.env.PORT || 3000));
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-// Server frontpage
-app.get('/', function (req, res) {
-    res.send('Big Messenger Bot');
-});
-
-// Facebook Webhook
-// Creates the endpoint for our webhook
+// Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {
 
+  // Parse the request body from the POST
   let body = req.body;
 
-  // Checks this is an event from a page subscription
+  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
-    // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
+
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
+      console.log('Sender ID: ' + sender_psid);
+
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
+
         handlePostback(sender_psid, webhook_event.postback);
       }
-    });
 
-    // Returns a '200 OK' response to all requests
+    });
+    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
+
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
+    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 
 });
-// Adds support for GET requests to our webhook
+
+// Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
 
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "customToken"
+  /** UPDATE YOUR VERIFY TOKEN **/
+  const VERIFY_TOKEN = "customToken";
 
-  // Parse the query params
+  // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
 
-  // Checks if a token and mode is in the query string of the request
+  // Check if a token and mode were sent
   if (mode && token) {
 
-    // Checks the mode and token sent is correct
+    // Check the mode and token sent are correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-      // Responds with the challenge token from the request
+      // Respond with 200 OK and challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
 
@@ -75,17 +78,63 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-// Handles messages events
 function handleMessage(sender_psid, received_message) {
   let response;
-  // Check if the message contains text
+
+  // Checks if the message contains text
   if (received_message.text) {
-    // Create the payload for a basic text message
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
     }
   }
-  // Sends the response message
+
+  // Send the response message
+  callSendAPI(sender_psid, response);
+}
+
+function handlePostback(sender_psid, received_postback) {
+  console.log('ok')
+   let response;
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
+  }
+  // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
 }
 
@@ -97,10 +146,11 @@ function callSendAPI(sender_psid, response) {
     },
     "message": response
   }
+
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": 'EAAD04ncSFgEBAPhp2o2kwc132nLwjItWjdD0dGcQuv2BIMsaA8JAhDqsVC5NGugWW7dhO3krwpOEaILdhWQvcELDJf6O2tiBBIIQTv6EFvFiVocuuZCD6XsNRNAL7WFSvpoMnYmDodXJJ3cSAxfz71KuraHWoZB1Q84nXsnMvhcFZBZCu1QyGKHhufMaZApYZD'},
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
@@ -110,14 +160,4 @@ function callSendAPI(sender_psid, response) {
       console.error("Unable to send message:" + err);
     }
   });
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-
 }
