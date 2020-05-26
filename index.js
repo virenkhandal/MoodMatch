683bdb7bc8a10fdf4a7cb950efa63880f8ac2ef2
Viren Khandal <virenkhandal@berkeley.edu>
2020-05-26 12:15:09 -0700

    updates

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.get('/', (request, response) => {
+return response.send('Ping!');
+});
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
 
