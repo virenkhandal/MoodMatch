2f52b48a7be39c2bb79bb6bb696f3e71a691f725
Viren Khandal <virenkhandal@berkeley.edu>
2020-05-26 17:18:49 -0700

    updates

--- index.js
+++ index.js
@@ -155,17 +155,27 @@ function handlePostback(sender_psid, received_postback) {
 
   // Set the response based on the postback payload
   if (payload === '<postback_payload>') {
-    response = { "text": "Hi! Let's find you some songs to fit your mood!",
-        "buttons": [
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Hi! Let's find you some songs to fit your mood!",
+            "buttons": [
               {
                 "type": "web_url",
                 "url": "https://google.com",
                 "title":"Select Criteria",
                 "webview_height_ratio": "full",
                 "messenger_extensions": true,
-                "fallback_url": "https://petersfancyapparel.com/fallback"
+                "fallback_url": "https://google.com"
               }
-        ]
+                ]
+            ],
+          }]
+        }
+      }
     }
   } else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
