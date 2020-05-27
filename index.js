05fdd42719600b94fb9c5df966fefa767da165fa
Viren Khandal <virenkhandal@berkeley.edu>
2020-05-26 17:15:13 -0700

    updates

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ function handlePostback(sender_psid, received_postback) {
 
   // Set the response based on the postback payload
   if (payload === '<postback_payload>') {
-    response = { "text": "Hi! Let's find you some songs to fit your mood!"
+    response = { "text": "Hi! Let's find you some songs to fit your mood!",
         "buttons": [
               {
                 "type": "web_url",
